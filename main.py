fb302ce4cd551146a8cce275fbbf4c4216e52694
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-04-30 22:09:42 +0500

    added:graphical_rep

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="Insurance Data Analysis",
     page_icon="📊",
     layout="wide",
-    initial_sidebar_state= "collapsed"
+    initial_sidebar_state= "expanded",
 )
 
 # Function to load and display SVG
